1e2376f738f05feb5e438ccee885c6f46fc5a408
Adib Pathan <121228160+adibpathan@users.noreply.github.com>
2024-10-08 17:46:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,13 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+// app.use(cookieParser());
+// const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
 
-app.use(cors(corsOptions));
+app.use(cors();
 
 const PORT = process.env.PORT || 3000;
 
