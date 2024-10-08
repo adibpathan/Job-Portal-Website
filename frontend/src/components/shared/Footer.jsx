7296bd0eea31c6422e992940e8c4509584270e55
Adib Pathan <121228160+adibpathan@users.noreply.github.com>
2024-10-08 18:39:16 +0530

    Update Footer.jsx

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FaInstagramSquare } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa6";
+// import { FaInstagramSquare } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa6";
+import { FaInstagramSquare, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
